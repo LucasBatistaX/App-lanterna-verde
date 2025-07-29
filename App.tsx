b282efd63d3ea259170efd7087f9c51e1b3016ea
Lucas Batista <Lucas915706@gmail.com>
2025-07-29 19:36:16 -0300

    My-First-App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react"
+import { StyleSheet, Image, View,TouchableOpacity } from "react-native";
+import symbolOn from "./assets/pictures/symbol-on.png";
+import symbolOff from "./assets/pictures/symbol-off.png";
 
 export default function App() {
+  const [isActive, setisActive] = useState(false)
+
+  const hadleSingle = ()=>{
+    setisActive((oldValue: boolean) => {
+      return !oldValue
+    })
+   }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={hadleSingle}>
+        <Image source={isActive ? symbolOn : symbolOff}></Image>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  containerOff:{
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  }
 });
---
+++ README.md
@@ -0,0 +1,52 @@
+# 🔦 Green Lantern App
+
+Um aplicativo simples desenvolvido com React Native e Expo, criado para treinar e dar os primeiros passos no desenvolvimento mobile.
+
+## 📌 Sobre o Projeto
+O Green Lantern App é um aplicativo com apenas uma tela, exibindo o símbolo do Lanterna Verde.
+Quando o usuário toca no símbolo, a lanterna do dispositivo é acionada, iluminando como um verdadeiro Guardião de Oa. 🌌
+
+Este é meu primeiro projeto em React Native, feito com o objetivo de aprender e explorar a criação de aplicativos móveis utilizando o Expo.
+
+## 🚀 Tecnologias Utilizadas
+
+- React Native
+
+- Expo
+
+##  Funcionalidades
+
+✅ Interface simples e intuitiva
+✅ Exibe o símbolo do Lanterna Verde
+✅ Ao clicar no símbolo, a lanterna do celular acende
+✅ Ao clicar novamente, a lanterna apaga
+
+▶️ Como executar o projeto
+
+Clone este repositório:
+
+
+git clone https://github.com/LucasBatistaX/App-lanterna-verde.git
+
+Acesse a pasta do projeto:
+
+cd my-app
+
+- Instale as dependências:
+
+npm install
+
+- Inicie o projeto com Expo:
+
+npx expo start
+
+- Inicie o App
+
+npm run start
+
+Escaneie o QR Code no terminal ou abra o projeto no seu emulador/dispositivo físico.
+
+✨ Autor
+
+👤 Lucas Batista (Licença MIT)
+🌐 LinkedIn: www.linkedin.com/in/lucas-batista-327704221
